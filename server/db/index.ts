e679b495a9d8220dc6cd1b9630184b595b2fc66d
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-21 17:19:33 -0600

    install sequelize
    no models or functionality yet, but not broken either

---
+++ server/db/index.ts
@@ -0,0 +1,10 @@
+// database access point (incl. associations)
+
+// import original db connection
+import db from './db';
+
+// define associations:
+// none yet
+
+// export db and models
+export { db };
