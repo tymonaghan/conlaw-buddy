643607624ea3eb9af16f7d95d01adb357f1f399c
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-21 01:06:09 -0600

    implement mui menu bar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // https://create-react-app.dev/docs/running-tests#writing-tests
 test('renders the "conlaw buddy" app name', () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
   const linkElement = screen.getByText(/conlaw-buddy/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the navbar', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const linkElement = screen.getByText(/MUI/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the React Router * route', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const linkElement = screen.getByText(
+    /React Router checking in. This is the \* route/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/Home.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-
-const Home = () => <Box>YO what up</Box>;
-
-export default Home;
