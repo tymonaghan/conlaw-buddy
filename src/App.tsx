0b00eacdd545b1b93a1c16561bf039f09cb527e6
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-21 01:16:12 -0600

    install react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Alert>This is a react-bootstrap alert.</Alert>
         <a
           className="App-link"
           href="https://reactjs.org"
